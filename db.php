515d7dd892cadcbef7846bbb3087e9859c044e31
Clare <clare.dowson@barnardos.org.uk>
2021-03-29 14:24:35 +0100

    data goot from database and appearing, need to get into a better format

--- db.php
+++ db.php
@@ -1,7 +1,5 @@
 <?php
 
 $dbconn = pg_connect("host=localhost port=5432 dbname=claretest");
-$result = pg_query($dbconn, "select * from quiz");
-var_dump(pg_fetch_all($result));
 
 ?>
--- viewquiz.php
+++ viewquiz.php
@@ -1,3 +1,4 @@
+<?php include_once 'db.php'; ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -16,7 +17,8 @@
 <body>
     <div class="container">
         <div id="home" class="flex__center flex__column">
-            <h1 class="title__item">What Colour is Kermit?</h1>
+          
+        <h1 class="title__item">What Colour is Kermit?</h1>
             <div>
                 <div class="choice__container">
                     <p class="choice__prefix">A</p>
@@ -43,3 +45,14 @@
     </div>
 </body>
 </html>
+<?php
+$db = pg_connect("host=localhost port=5432 dbname=claretest");
+$result = pg_query($db,"SELECT * FROM quiz");
+echo "<table>";
+while($row=pg_fetch_assoc($result)){echo "<tr>";
+echo "<td align='center' width='300'>" . $row['question'] . "</td>";
+echo "<td align='center' width='200'>" . $row['choice_1'] . "</td>";
+echo "<td align='center' width='200'>" . $row['choice_2'] . "</td>";
+echo "<td align='center' width='200'>" . $row['choice_3'] . "</td>";
+echo "<td align='center' width='200'>" . $row['choice_4'] . "</td>";
+echo "</tr>";}echo "</table>";?>
