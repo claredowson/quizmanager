3a63f20b8e607f21003487220ef44718fd446d87
Clare <clare.dowson@barnardos.org.uk>
2021-04-12 15:24:23 +0100

    validation working, need to clear results when it goes back to the screen

--- adminadd.php
+++ adminadd.php
@@ -1,21 +1,25 @@
 <?php include_once 'db.php'; 
 
-$fieldErrors = array();
-    if(isset($_POST['Done']))
-    {
-        $fields = array('question', 'choice_1', 'choice_2', 'choice_3', 'choice_4', 'answer');
-        foreach ($fields as $field) 
-        {
-            if ($_POST[$field] == "")  
-            {
-                $fieldErrors[] = $field;
-            } 
+$errors = [];
+$fields = ['question', 'choice_1', 'choice_2', 'choice_3', 'choice_4', 'answer'];
+$optionalFields = ['answer'];
+$values = [];
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    foreach ($fields as $field) {
+        if (empty($_POST[$field]) && !in_array($field, $optionalFields)) {
+            $errors[] = $field;
+        } else {
+            $values[$field] = $_POST[$field];
         }
     }
 
-    if($fieldErrors == "")
-    print_r($fieldErrors);
-
+        if (empty($errors)) {
+            $query = "INSERT
+            INTO quiz (question, choice_1, choice_2, choice_3, choice_4, answer)
+            VALUES ('$_POST[question]','$_POST[choice_1]','$_POST[choice_2]','$_POST[choice_3]','$_POST[choice_4]','$_POST[anwser]')";
+            $result = pg_query($query);
+        }
+}
 ?>
 
 <!DOCTYPE html>
@@ -38,25 +42,30 @@ $fieldErrors = array();
             <h2>Enter a new question into the Nature Quiz</h2>
             <ul>
                 <form name="insert" method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-                    <li>Question:</li><li><input type="text" id="file" name="question"/> 
-                    <span id="error_name" style="color:red;">
-                    <?php echo ($fieldErrors && in_array("question", $fieldErrors)) ? 'Please enter a question' : ''; ?></span>  
+                    <li>Question:</li><li><input type="text" id="file" name="question" value="<?php echo htmlspecialchars($values['question']);?>"/> 
+                    <?php if (in_array('question', $errors)): ?>
+                    <span class="error">Please enter a question</span>
+                    <?php endif; ?> 
                     </li>
-                    <li>Choice 1:</li><li><input type="text" id="file"  name="choice_1" />
-                    <span id="error_name" style="color:red;">
-                    <?php echo (count($fieldErrors) > 0 && in_array("choice_1", $fieldErrors)) ? 'Please enter a choice' : ''; ?></span> 
+                    <li>Choice 1:</li><li><input type="text" id="file"  name="choice_1" value="<?php echo htmlspecialchars($values['choice_1']);?>"/>
+                    <?php if (in_array('question', $errors)): ?>
+                    <span class="error">Please enter a choice</span>
+                    <?php endif; ?> 
                     </li>
-                    <li>Choice 2:</li><li><input type="text" id="file" name="choice_2" />
-                    <span id="error_name" style="color:red;">
-                    <?php echo (count($fieldErrors) > 0 && in_array("choice_1", $fieldErrors)) ? 'Please enter a choice' : ''; ?></span>
+                    <li>Choice 2:</li><li><input type="text" id="file" name="choice_2" value="<?php echo htmlspecialchars($values['choice_2']);?>"/>
+                    <?php if (in_array('question', $errors)): ?>
+                    <span class="error">Please enter a choice</span>
+                    <?php endif; ?> 
                     </li>
-                    <li>Choice 3:</li><li><input type="text" id="file"  name="choice_3" />
-                    <span id="error_name" style="color:red;">
-                    <?php echo (count($fieldErrors) > 0 && in_array("choice_1", $fieldErrors)) ? 'Please enter a choice' : ''; ?></span>
+                    <li>Choice 3:</li><li><input type="text" id="file"  name="choice_3" value="<?php echo htmlspecialchars($values['choice_3']);?>" />
+                    <?php if (in_array('question', $errors)): ?>
+                    <span class="error">Please enter a choice</span>
+                    <?php endif; ?> 
                     </li>
-                    <li>Choice 4:</li><li><input type="text" id="file"  name="choice_4" />
-                    <span id="error_name" style="color:red;">
-                    <?php echo (count($fieldErrors) > 0 && in_array("choice_1", $fieldErrors)) ? 'Please enter a choice' : ''; ?></span>
+                    <li>Choice 4:</li><li><input type="text" id="file"  name="choice_4"  value="<?php echo htmlspecialchars($values['choice_4']);?>"/>
+                    <?php if (in_array('question', $errors)): ?>
+                    <span class="error">Please enter a choice</span>
+                    <?php endif; ?> 
                     </li>
                     <li>Answer:</li>
                     <li>  
@@ -73,9 +82,3 @@ $fieldErrors = array();
     </div>
 </body>
 </html>
-<?php  
-
-$query = "INSERT INTO quiz (question, choice_1, choice_2, choice_3, choice_4, answer) VALUES ('$_POST[question]','$_POST[choice_1]','$_POST[choice_2]','$_POST[choice_3]','$_POST[choice_4]','$_POST[anwser]')"; 
-$result = pg_query($query);  
-
-?> 
