b6515862e18296db018464b0059e262b4f494411
Clare <clare.dowson@barnardos.org.uk>
2021-04-13 11:47:50 +0100

    delete working, just not refreshing straight and not overly clear what is being deleted

--- admindeleting.php
+++ admindeleting.php
@@ -1,4 +1,6 @@
 <?php include_once 'db.php'; ?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -20,19 +22,33 @@
             <h1 class="title__item">Nature Quiz</h1>
             <?php
             $db = pg_connect("host=localhost port=5432 dbname=claretest");
-            $result = pg_query($db,"SELECT question FROM quiz"); 
+            $result = pg_query($db,"SELECT id, question FROM quiz"); 
             echo "<table>";
             echo "<tbody>";
             echo "<tr>";
             while($row=pg_fetch_assoc($result)){
-            echo "<th scope='col' class='choice__prefix'>Delete</th>";
-            echo "<td align='left' width='100'  class='question'>" . 
-            $row['question'] . "</td>";
+            $questions[] = ['id' => $row['id'], 'question' => 
+            $row['question']]; "</td>";
             echo "</tr>";
             echo "</tbody>";
             }echo"</table>";?>
-                <div>
-            <h1 class="title__item">What animal has the longest hair?</h1>
+
+<?php foreach ($questions as $question): ?>
+<blockquote>
+    <p>
+    <?=htmlspecialchars($question['question'], 
+        ENT_QUOTES, 'UTF-8')?>
+    <form name="deletequestion" method="POST">
+        <input type="hidden" name="id" value="<?=$question['id']?>">
+        <input type="submit" value="Delete">
+    </form>
+    </p>
+</blockquote>
+<?php endforeach; 
+$id = $_POST['id'];
+$query = "DELETE FROM quiz WHERE id='$id'";
+$result = pg_query($query);
+?>
             <div>
                 <a href="admindelete.php">Delete a question from another Quiz</a>
                 <a href="adminlogin.php">Back to login page</a>
