79afa0bf97e17d06e0e2f542fb2d37d8022a25f5
Clare <clare.dowson@barnardos.org.uk>
2021-04-16 10:18:19 +0100

    wip play quiz

--- resplay.php
+++ resplay.php
@@ -1,4 +1,5 @@
-<?php include_once 'db.php'; ?>
+<?php include_once 'db.php'; 
+session_start(); ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -19,21 +20,14 @@
         <div id="home" class="flex__center flex__column">
             <h1 class="title__item">Nature Quiz</h1>
  
-    <?php   
-														
+    <?php  
+    
+    $_SESSION['clicks'] = 0;
+												
 	if (isset($_POST['click']) || isset($_GET['start'])) {
-		@$_SESSION['clicks'] += 1 ;
-		$c = $_SESSION['clicks'];
-		if(isset($_POST['userans'])) { $userselected = $_POST['userans'];
-																
-		$fetchqry2 = "UPDATE `quiz` SET `answer`='$userselected' WHERE `id`=$c-1"; 
-		
-		}
-		  
-																	
- 		} else {
-			$_SESSION['clicks'] = 0;
-		}
+		$_SESSION['clicks'] += 1 ;
+        
+    }
 												
 ?>
 
@@ -48,26 +42,20 @@ $row=pg_fetch_assoc($result);
 </h3></td></tr> <?php if($_SESSION['clicks'] > 0 && $_SESSION['clicks'] < 6){ ?>
 
     <tr><td><h3><br><?php echo @$row['question'];?>     
-            <tr><td><input required type="radio" name="userans" class="choices" value="<?php echo $row['choice_1'];?>">&nbsp;<?php echo $row['choice_1'];?><br>
-            <tr><td><input required type="radio" name="userans" class="choices" value="<?php echo $row['choice_2'];?>">&nbsp;<?php echo $row['choice_2'];?><br>
-            <tr><td><input required type="radio" name="userans" class="choices" value="<?php echo $row['choice_3'];?>">&nbsp;<?php echo $row['choice_3'];?><br>
-            <tr><td><input required type="radio" name="userans" class="choices" value="<?php echo $row['choice_4'];?>">&nbsp;<?php echo $row['choice_4'];?><br>
-            <tr><td><button class="button" name="click" >Next</button></td></tr> <?php } ?>
+            <tr><td><input required type="radio" class="choices" value="<?php echo $row['choice_1'];?>">&nbsp;<?php echo $row['choice_1'];?><br>
+            <tr><td><input required type="radio" class="choices" value="<?php echo $row['choice_2'];?>">&nbsp;<?php echo $row['choice_2'];?><br>
+            <tr><td><input required type="radio" class="choices" value="<?php echo $row['choice_3'];?>">&nbsp;<?php echo $row['choice_3'];?><br>
+            <tr><td><input required type="radio" class="choices" value="<?php echo $row['choice_4'];?>">&nbsp;<?php echo $row['choice_4'];?><br>
+            <tr><td><button class="button" name="click">Next</button></td></tr> <?php } ?>
        
      
        <form>
        <?php if($_SESSION['clicks']>5){ 
-          $qry3 = "SELECT `answer`, 'userans' FROM `quiz`;";
-          $result3 = pg_query($con,$qry3);
-          $storeArray = Array();
-          while ($row3 = pg_fetch_assoc($result3)) {
-           if($row3['answer']==$row3['userans']){
-               @$_SESSION['score'] += 1 ;
-           }
-      }
-    }
        
-       ?> 
+    } 
+    ?>
+       
+       
             
             <div>
                 <a href="resend.php">Temp Link</a>
