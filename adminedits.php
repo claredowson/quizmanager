098e93a34806e96b5b30da2a4b7586b5b79cfb37
Clare <clare.dowson@barnardos.org.uk>
2021-04-20 16:08:42 +0100

    wip editing a question

--- adminedits.php
+++ adminedits.php
@@ -1,11 +1,14 @@
-<?php include_once 'db.php'; ?>
+<?php include_once 'db.php'; 
+$db = pg_connect("host=localhost port=5432 dbname=claretest");
+$result = pg_query($db,"SELECT * FROM quiz");
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>quiz</title>
+    <title>Edit Questions</title>
     <link rel="stylesheet" href="app.css">
     <link rel="stylesheet" href="resplay.css">
 </head>
@@ -15,59 +18,42 @@
     </figure>
 </header>
 <body>
-    <div class="container">
-        <div id="home" class="flex__center flex__column">
-        <?php
-            $db = pg_connect("host=localhost port=5432 dbname=claretest");
-            $result = pg_query($db,"SELECT * FROM quiz"); 
-            echo "<table>";
-            echo "<tr>";
-            while($row=pg_fetch_assoc($result)){
-            echo "</tr>";
-            echo "<td class='question'>" .$row['question'] . "</td>";
-            echo "<td class='choice_1'>" .$row['choice_1'] . "</td>";
-            echo "<td class='choice_2'>" .$row['choice_2'] . "</td>";
-            echo "<td class='choice_3'>" .$row['choice_3'] . "</td>";
-            echo "<td class='choice_4'>" .$row['choice_4'] . "</td>";
-            echo "<td class='answer'>" .$row['answer'] . "</td>";
-            }echo"</table>";?>
+<?php
+if (pg_num_rows($result) > 0) {
+?>
+<table>
 
-
-<blockquote>
-    <p>
-    <?=htmlspecialchars($question['question'], 
-        ENT_QUOTES, 'UTF-8');?>
-    <form name="editquestion" method="POST">
-        <input type="hidden" name="id" value="<?=$question['id']?>">
-        <input type="submit" value="Edit">
-    </form>
-    </p>
-</blockquote>
-
-            <h1 class="title__item">What Colour is Kermit?</h1>
-            <div>
-                <div class="choice__container">
-                    <p class="choice__prefix">A</p>
-                    <p class="choice__text">Red</p>
-                </div>
-                <div class="choice__container">
-                    <p class="choice__prefix">B</p>
-                    <p class="choice__text">Pink</p>
-                </div>
-                <div class="choice__container">
-                    <p class="choice__prefix">C</p>
-                    <p class="choice__text">Green</p>
-                </div>
-                <div class="choice__container">
-                    <p class="choice__prefix">D</p>
-                    <p class="choice__text">Blue</p>
-                </div>
-            </div>
+	  <tr>
+	    <td>Question</td>
+		<td>Action</td>
+	  </tr>
+			<?php
+			$i=0;
+			while($row = pg_fetch_array($result)) {
+			?>
+	  <tr>
+		<td><?php echo $row["question"]; ?></td>
+		<td><a href="update-process.php?id=<?php echo $row["id"]; ?>">Update</a></td>
+      </tr>
+      <?php
+			$i++;
+			}
+			?>
+</table>
+<?php
+}
+else
+{
+    echo "No result found";
+}
+?>
+  
+   
             <div>
                 <a href="adminedit.php">Edit another Quiz</a>
                 <a href="adminlogin.php">Back to login page</a>
             </div>
-        </div>
-    </div>
+        
+   
 </body>
 </html>
---
+++ update-process.php
@@ -0,0 +1,62 @@
+<?php include_once 'db.php'; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Updating Questions</title>
+    <link rel="stylesheet" href="app.css">
+    <link rel="stylesheet" href="resplay.css">
+</head>
+<header>
+    <figure class="logo">
+        <img src="images/barnardos.png" alt="Barnardos Logo" />
+    </figure>
+</header>
+
+<?php
+if(count($_POST)>0) {
+pg_query($db,"UPDATE quiz set id='" . $_POST['id'] . "', question='" . $_POST['question'] . "', choice_1='" . $_POST['choice_1'] . "', choice_2='" . $_POST['choice_2'] . "' ,choice_3='" . $_POST['choice_3'] . "', choice_4='" . $_POST['choice_4'] . "', answer='" . $_POST['answer'] . "'  WHERE id='" . $_POST['id'] . "'");
+$message = "Record Modified Successfully";
+}
+$result = pg_query($db,"SELECT * FROM quiz WHERE id='" . $_GET['id'] . "'");
+$row= pg_fetch_array($result);
+?>
+
+
+<head>
+    <title>Update Question Data</title>
+</head>
+<body>
+    <form name="frmUser" method="post" action="">
+        <div><?php if(isset($message)) { echo $message; } ?>
+        </div>
+<div style="padding-bottom:5px;">
+<a href="adminedits.php">Question List</a>
+</div>
+
+<br>
+Question: <br>
+<input type="text" question="question" class="txtField" value="<?php echo $row['question']; ?>">
+<br>
+Choice 1 :<br>
+<input type="text" name="choice_1" class="txtField" value="<?php echo $row['choice_1']; ?>">
+<br>
+Choice 2:<br>
+<input type="text" name="choice_2" class="txtField" value="<?php echo $row['choice_2']; ?>">
+<br>
+Choice 3:<br>
+<input type="text" name="choice_3" class="txtField" value="<?php echo $row['choice_3']; ?>">
+<br>
+Choice 4:<br>
+<input type="text" name="choice_4" class="txtField" value="<?php echo $row['choice_4']; ?>">
+<br>
+Answer:<br>
+<input type="text" name="answer" class="txtField" value="<?php echo $row['answer']; ?>">
+<br>
+<input type="submit" name="submit" value="Submit" class="buttom">
+
+</form>
+</body>
+</html>
