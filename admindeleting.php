820e3243b9ad0a29120ccb9283b3e253c1e8c976
Clare <clare.dowson@barnardos.org.uk>
2021-04-26 20:33:18 +0100

    wip deleting questions

--- admindeleting.php
+++ admindeleting.php
@@ -19,36 +19,30 @@
     <div class="container">
         <div id="home" class="flex__center flex__column">
             <h1 class="title__item">Nature Quiz</h1>
-            <?php
+
+          
+            <?php 
             $db = pg_connect("host=localhost port=5432 dbname=claretest");
             $result = pg_query($db,"SELECT id, question FROM quiz"); 
             echo "<table>";
-            echo "<tbody>";
             echo "<tr>";
-            while($row=pg_fetch_assoc($result)){
-            $questions[] = ['id' => $row['id'], 'question' => 
-            $row['question']]; "</td>";
-            echo "</tr>";
-            echo "</tbody>";
-            }echo"</table>";?>
+            while($row=pg_fetch_assoc($result)){ echo "<tr>";
+               echo "<th>" . $row['question'] . "</th>";
+               echo "<td>" . " <input type='submit' class='button' name='Delete' value='Delete'  /></td>";
+               echo "</tr>";
+            }echo"</table>"; ?>
+            
+        
+     
+            <form name="delete" method="POST" action="">
+       
+                <?php
+                $id = $_POST['id'];
+                $query = "DELETE FROM quiz WHERE id='$id'";
+                pg_query($query);
+                ?>
+            </form>
 
-<?php foreach ($questions as $question): ?>
-<blockquote>
-    <p>
-    <?=htmlspecialchars($question['question'], 
-        ENT_QUOTES, 'UTF-8');?>
-    <form name="deletequestion" method="POST">
-        <input type="hidden" name="id" value="<?=$question['id']?>">
-        <input type="submit" value="Delete">
-    </form>
-    </p>
-</blockquote>
-<?php endforeach; 
-
-$id = $_POST['id'];
-$query = "DELETE FROM quiz WHERE id='$id'";
-pg_query($query);
-?>
 
             <div>
                 <a href="admindelete.php">Delete a question from another Quiz</a>
@@ -58,3 +52,4 @@ pg_query($query);
     </div>
 </body>
 </html>
+
