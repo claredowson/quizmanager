456957da04896b596560d4720c921d6b22eb3f9b
Clare <clare.dowson@barnardos.org.uk>
2021-04-29 15:39:02 +0100

    wip hashing

--- post_login.php
+++ post_login.php
@@ -7,8 +7,31 @@ $db = pg_connect("host=localhost port=5432 dbname=claretest");
 $username = $_POST['username'];
 $password = $_POST['password'];
 
+$query="SELECT * FROM users WHERE username='$username' and password='$password'";
+$result =pg_query($query);
+
+$count=pg_num_rows($result);
+
+if($count==1){
+
 $hashed_password = password_hash($password, PASSWORD_DEFAULT);
 var_dump($hashed_password);
+
+$stmt = $dbconn->prepare("UPDATE users 
+SET password = VALUES (?)
+WHERE username='". $username."'");
+
+$stmt-> bind_param ("s", $hashed_password);
+$result = $stmt->execute();
+}
+else {
+    $msg = "You left one or more of the required fields.";
+    echo "Wrong Username or password";
+    
+}
+
+
+// if username is in the database, hash password and update table using username and updating hashed password
 
 $query="SELECT * FROM users WHERE username='$username' and password='$hashed_password'";
 $result =pg_query($query);
@@ -24,7 +47,9 @@ if($count==1){
             header("location:login_success.php");
         }
         else {
+            $msg = "You left one or more of the required fields.";
             echo "Wrong Username or password";
+            
         }
 
 
